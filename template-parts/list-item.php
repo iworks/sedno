1b8c2704477951157e226c332d099d9aee0b41e6
Marcin Pietrzak <marcin@iworks.pl>
2021-12-27 20:49:52 +0100

    betrter thumbs on lists

--- template-parts/list-item.php
+++ template-parts/list-item.php
@@ -8,20 +8,39 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
 <?php
-the_post_thumbnail(
-	'list',
-	array(
-		'alt' => the_title_attribute(
-			array(
-				'echo' => false,
-			)
-		),
-	)
-);
-?>
+if ( has_post_thumbnail() ) {
+
+
+
+	$attachment_id = get_post_thumbnail_id( get_the_ID(), 'list' );
+	if ( $attachment_id ) {
+		$styles = array();
+		$data   = '';
+
+		$value = apply_filters( 'iworks_aggresive_lazy_load_get_dominant_color', null, $attachment_id );
+		if ( ! empty( $value ) ) {
+			$styles[] = sprintf( 'background-color:#%s', $value );
+		}
+		$value = apply_filters( 'iworks_aggresive_lazy_load_get_tiny_thumbnail', null, $attachment_id );
+		if ( ! empty( $value ) ) {
+			$styles[] = sprintf( 'background-image:url(%s)', $value );
+		}
+		$data .= sprintf(
+			' data-src="%s"',
+			esc_url( get_the_post_thumbnail_url( get_the_ID() ) )
+		);
+		printf(
+			'<a class="post-thumbnail" href="%s" style="%s"%s aria-hidden="true" tabindex="-1"></a>',
+			esc_url( get_permalink() ),
+			esc_attr( implode( ';', $styles ) ),
+			$data
+		);
+	}
+	?>
 </a>
+<?php } else { ?>
+<?php } ?>
 	<div class="entry-content">
 		<header class="entry-header">
 			<div class="entry-meta"><?php sedno_posted_on(); ?></div>
