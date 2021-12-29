ad136762fe5b05963eb6c6497527f68be4bdc30a
Marcin Pietrzak <marcin@iworks.pl>
2021-12-29 16:28:14 +0100

    add old comments links

--- archive-editorial_comment.php
+++ archive-editorial_comment.php
@@ -19,6 +19,9 @@ get_header();
 				the_post();
 				get_template_part( 'template-parts/list-item', get_post_type() );
 			}
+			?>
+<p style="text-align:center"><a href="https://sedno.org/ogloszenie/komentarze-redakcyjne-dziennika-sedno-z-lat-2018-2020/">Komentarze opublikowane przed 10.11.2021</a> znajdują się w pliku zbiorczym w zakładce <a href="https://sedno.org/ogloszenia/">Ogłoszenia</a>.</p>
+			<?php
 			do_action( 'sedno_the_posts_navigation' );
 		} else {
 			get_template_part( 'template-parts/content', 'none' );
