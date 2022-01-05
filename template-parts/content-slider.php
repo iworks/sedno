9676b26d246290c147f7ba4a7eedfc7af6b5fda7
Marcin Pietrzak <marcin@iworks.pl>
2022-01-05 14:36:46 +0100

    hamburger & date on slider

--- template-parts/content-slider.php
+++ template-parts/content-slider.php
@@ -9,9 +9,10 @@ if ( isset( $args['index'] ) && 0 === $args['index'] ) {
 <a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
 	<article id="post-<?php the_ID(); ?>" class="<?php echo $class; ?>" >
 		<div class="entry-wrap">
-		<div class="entry" style="z-index:<?php echo $z_index + 1; ?>">
+			<div class="entry" style="z-index:<?php echo $z_index + 1; ?>">
+				<div class="entry-meta"><?php sedno_posted_on(); ?></div>
 				<header class="entry-header"><?php the_title( '<h2 class="entry-title">', '</h2>' ); ?></header>
-				<div class="entry-meta">
+				<div class="entry-meta entry-meta-author">
 				<?php
 				if ( function_exists( 'coauthors' ) ) {
 					coauthors();
